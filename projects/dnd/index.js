8eb8824386e9980756e5344fe81149b94d921b12
Elena Kuleshova <elenta1511@gmail.com>
2021-06-26 19:17:11 +0300

    'dnd'

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,43 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+function randomNum(from, to) {
+  return parseInt(from + Math.random() * to - from);
+}
 
-export function createDiv() {}
+let dd;
+let coordY = 0;
+let coordX = 0;
+
+document.addEventListener('mousemove', (e) => {
+  if (dd) {
+    dd.style.top = e.clientY - coordY + 'px';
+    dd.style.left = e.clientX - coordX + 'px';
+  }
+});
+
+export function createDiv() {
+  const div = document.createElement('div');
+  const min = 100;
+  const max = 100;
+  const color = '0xffffff';
+
+  div.className = 'draggable-div';
+  div.style.top = randomNum(0, window.innerHeight) + 'px';
+  div.style.left = randomNum(0, window.innerWidth) + 'px';
+  div.style.width = randomNum(min, max) + 'px';
+  div.style.height = randomNum(min, max) + 'px';
+  div.style.background = '#' + randomNum(0, color).toString(16);
+
+  div.addEventListener('mousedown', (e) => {
+    dd = div;
+    coordY = e.offsetY;
+    coordX = e.offsetX;
+  });
+  div.addEventListener('mouseup', () => (dd = false));
+
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
